f1b692c9bb987800a12c4c5a97a7d4049af646f2
Simone Benatti <simone.benatti@studenti.unipr.it>
2020-02-18 12:10:43 +0100

    No sleep, no MKL install, added CMake log to artifacts

--- contrib/packaging-python/gitlab/build_pychrono.bat
+++ contrib/packaging-python/gitlab/build_pychrono.bat
@@ -2,7 +2,7 @@
 echo Process begins
 call "%CONDA_INSTALL_LOCN%"\Scripts\activate.bat
 call conda install --yes conda-build anaconda-client
-call conda install --yes -c intel mkl-devel=2018.0.3
+Rem call conda install --yes -c intel mkl-devel=2018.0.3
 call conda build .\contrib\packaging-python\conda --python=3.6 --no-remove-work-dir --dirty
 call anaconda --token "%ANACONDA_TOKEN%" upload "%CONDA_INSTALL_LOCN%"\conda-bld\pychrono-4.0.0*.bz2
 echo "End Reached"
