833c2c76dffb319dc40c3e4fe1c0163ce6cfc658
Simone Benatti <simone.benatti@studenti.unipr.it>
2020-02-18 19:18:19 +0100

    Mixed approach

--- contrib/packaging-python/gitlab/build_pychrono.bat
+++ contrib/packaging-python/gitlab/build_pychrono.bat
@@ -3,6 +3,8 @@ echo Process begins
 call "%CONDA_INSTALL_LOCN%"\Scripts\activate.bat
 call conda install --yes conda-build anaconda-client
 Rem call conda install --yes -c intel mkl-devel=2018.0.3
-call conda build .\contrib\packaging-python\conda --python=3.6 --no-remove-work-dir --dirty
-call anaconda --token "%ANACONDA_TOKEN%" upload "%CONDA_INSTALL_LOCN%"\conda-bld\pychrono-4.0.0*.bz2
+call conda install -c conda-forge swig=4.0 --yes
+call conda install mkl --yes
+Rem call conda build .\contrib\packaging-python\conda --python=3.6 --no-remove-work-dir --dirty
+Rem call anaconda --token "%ANACONDA_TOKEN%" upload "%CONDA_INSTALL_LOCN%"\conda-bld\pychrono-4.0.0*.bz2
 echo "End Reached"
