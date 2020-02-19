6c6446b05dce583a258f5b42d9635e2cd7dc93b7
Simone Benatti <simone.benatti@studenti.unipr.it>
2020-02-19 14:50:02 +0100

    install jinja

--- contrib/packaging-python/gitlab/build_pychrono.bat
+++ contrib/packaging-python/gitlab/build_pychrono.bat
@@ -6,6 +6,7 @@ Rem call conda install --yes -c intel mkl-devel=2018.0.3
 call conda install -c conda-forge swig=4.0 --yes
 call conda install mkl --yes
 call conda install cmake --yes
+call conda install jinja2 --yes
 call conda build .\contrib\packaging-python\conda --python=3.7 --no-remove-work-dir --dirty
 Rem call anaconda --token "%ANACONDA_TOKEN%" upload "%CONDA_INSTALL_LOCN%"\conda-bld\pychrono-4.0.0*.bz2
 echo "End Reached"
